60e32c190430df123c573562c0c5d65efc1a517b
samadizajn <info@samadizajn.com>
2015-03-27 10:28:25 +0100

    Triangle Gume Root

---
+++ root/assets/js/jquery.scripts.js
@@ -0,0 +1,34 @@
+/* Jquery scripts */
+jQuery( document ).ready(function( $ ) {
+
+
+
+
+
+
+
+
+
+
+// Ubuntu Google Font
+// font-family: 'Ubuntu', sans-serif;
+
+  WebFontConfig = {
+    google: { families: [ 'Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic:cyrillic-ext,latin,greek-ext,greek,latin-ext,cyrillic' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+
+
+
+
+
+}); /* // Jquery scripts */
